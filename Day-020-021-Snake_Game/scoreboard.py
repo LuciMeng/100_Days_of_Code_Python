51b528d0a3774603cde9f2e8bfce3427feb2f8c6
Ying M. <126923725+LuciMeng@users.noreply.github.com>
2026-06-06 12:12:37 -0700

    Add high score tracking and reset

--- Day-020-021-Snake_Game/scoreboard.py
+++ Day-020-021-Snake_Game/scoreboard.py
@@ -1,22 +1,32 @@
 from turtle import Turtle
-ALIGHTMENT = "center"
+ALIGNMENT = "center"
 FONT = ("Courier", 20, "bold")
+with open("data.txt") as data_file:
+    data = data_file.read()
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = int(data)
         self.penup()
         self.color('white')
         self.hideturtle()
         self.goto(0,280)
-        self.write(arg=f'Score: {self.score}', move=False, align=ALIGHTMENT, font=FONT)
+        self.score_update()
 
     def score_update(self):
         self.clear()
-        self.score += 1
-        self.write(arg=f'Score: {self.score}', move=False, align=ALIGHTMENT, font=FONT)
+        self.write(arg=f'Score: {self.score}  High Score: {self.high_score}', move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(arg=f'GAVE OVER :(', move=False, align=ALIGHTMENT, font=FONT)
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data_file:
+                data_file.write(str(self.high_score))
+        self.score = 0
+        self.score_update()
+
+    def increase_score(self):
+        self.score += 1
+        self.score_update()
